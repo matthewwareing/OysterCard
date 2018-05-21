c633dc630017524369e9fc98c69fe92182706edb
Matthew <mdwareing@gmail.com>
2018-05-21 15:36:24 +0100

    Chapter 8

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,18 +1,32 @@
 class Oystercard
     DEFAULT_BALANCE = 0
     MAX_BALANCE = 90
-    attr_reader :balance
-
+    attr_reader :balance, :in_use
     def initialize
         @balance = DEFAULT_BALANCE
+        @in_use = false
     end
     
     def top_up(amount)
-        fail "You can't top up more than £90" if balance + amount > MAX_BALANCE
+        fail "You can't top up more than £#{MAX_BALANCE}" if balance + amount > MAX_BALANCE
         @balance += amount
     end
 
     def deduct(amount)
         @balance -= amount
+    end
+
+    def in_journey?
+        @in_use
+    end
+
+    def touch_in
+        fail "You can't touch in before you've touched out!" if in_journey? == true
+        @in_use = true
+    end
+
+    def touch_out
+        fail "You can't touch out before you've touched in!" if in_journey? == false
+        @in_use = false
     end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,22 +2,64 @@ require 'oystercard'
 
 describe Oystercard do
     subject(:oystercard) { described_class.new }
-    it 'I want money on my card' do
-        expect(oystercard.balance).to eq 0
+
+    describe '#balance' do
+        it 'default value is zero' do
+            expect(oystercard.balance).to eq 0
+        end
     end
 
-    it '#top_up' do
-        expect { oystercard.top_up(10) }.to change { oystercard.balance }.by 10
+    describe '#top_up' do
+        it 'allows topping up' do
+            expect { oystercard.top_up(1) }.to change { oystercard.balance }.by 1
+        end
+    
+        it 'raises error when max balance is exceeded' do
+            max_balance = Oystercard::MAX_BALANCE
+            oystercard.top_up(max_balance)
+            expect{ oystercard.top_up(1) }.to raise_error "You can't top up more than £#{max_balance}"
+        end
     end
 
-    it '#top_up raises error when max balance is exceeded' do
-        max_balance = Oystercard::MAX_BALANCE
-        oystercard.top_up(max_balance)
-        expect{ oystercard.top_up(1) }.to raise_error "You can't top up more than £#{max_balance}"
+    describe '#deduct' do
+        it 'allows deducting balance' do
+            oystercard.top_up(2)
+            expect { oystercard.deduct(1)}.to change { oystercard.balance }.by -1 
+        end
     end
 
-    it '#deduct' do
-        oystercard.top_up(20)
-        expect { oystercard.deduct(10)}.to change { oystercard.balance }.by -10 
+    describe 'Journeys' do
+
+        context 'Journey has not started' do
+            it '#in_journey?' do
+                expect(oystercard.in_journey?).to eq false
+            end
+
+            it '#touch_in' do
+                oystercard.touch_in
+                expect(oystercard.in_use).to eq true
+            end
+
+            it '#touch_out' do
+                expect {oystercard.touch_out }.to raise_error "You can't touch out before you've touched in!"
+            end
+        end
+
+        context 'Journey has started' do
+            before { allow(oystercard).to receive(:in_journey?).and_return(true) }
+
+            it '#in_journey?' do
+                expect(oystercard.in_journey?).to eq true
+            end
+
+            it '#touch_in' do
+                expect { oystercard.touch_in }.to raise_error "You can't touch in before you've touched out!"
+            end
+
+            it '#touch_out' do
+                oystercard.touch_out
+                expect(oystercard.in_use).to eq false
+            end
+        end
     end
 end
