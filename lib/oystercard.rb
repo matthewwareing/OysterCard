febd38bb20decde5890a060eb75270f8e7b02b73
Matthew <mdwareing@gmail.com>
2018-05-21 14:25:26 +0100

    Chapter 5

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,12 @@
 class Oystercard
     DEFAULT_BALANCE = 0
     attr_reader :balance
+
     def initialize(balance: DEFAULT_BALANCE)
         @balance = balance
+    end
+    
+    def top_up(amount)
+        @balance += amount.abs
     end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,6 +3,10 @@ require 'oystercard'
 describe Oystercard do
     subject(:oystercard) { described_class.new }
     it 'I want money on my card' do
-        expect(subject.balance).to eq 0
+        expect(oystercard.balance).to eq 0
+    end
+
+    it '#top_up' do
+        expect { oystercard.top_up(10) }.to change { oystercard.balance }.by 10
     end
 end
