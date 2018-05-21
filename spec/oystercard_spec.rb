8b6faba55441a9b56978c0373574a7ef035b6056
Matthew <mdwareing@gmail.com>
2018-05-21 14:03:23 +0100

    Challenge: 4

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
+    subject(:oystercard) { described_class.new }
+    it 'I want money on my card' do
+        expect(subject.balance).to eq 0
+    end
 end
